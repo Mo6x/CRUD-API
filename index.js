4c8e8f81bd3e509700fef9e28b13e5f3e124be58
christopher-moses <christmoses1999@icloud.com>
2022-10-05 10:54:11 +0100

    I have create my server on PORT 5000

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello from express"));
+
+app.listen(PORT, () => console.log(`server runing on port: http://localhost:${PORT}`));
