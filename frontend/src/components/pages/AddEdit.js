a6a15913f280095c81b562be2e6ea6466c59b91e
christopher-moses <christmoses1999@icloud.com>
2022-10-09 14:38:31 +0100

    I have build the detail, where you can enter your email name and contact

--- frontend/src/components/pages/AddEdit.js
+++ frontend/src/components/pages/AddEdit.js
@@ -1,15 +1,70 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import './AddEdit.css';
 
 
-
+const initialState = {
+    name: "",
+    email: "",
+    contact: "",
+};
 
 
 export const AddEdit = () => {
-  return (
-      <div>
-          AddEdit
-      </div>
-  )
-}
+    const [state, setState] = useState(initialState);
+
+    const { name, email, contact } = initialState;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleInputChange = (e) => {
+        let { name, value } = e.target;
+        setState({ ...state, [name]: value });
+    };
+
+    return (
+        <div style={{ marginTop: "100px" }}>
+            <form style={{
+                margin: "auto",
+                padding: "15px",
+                maxWidth: "400px",
+                alignContent: "center",
+            }}>
+                onSubmit={handleSubmit}
+             
+                <label htmlFor='name'>Name</label>
+                <input 
+                type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter Name ..."
+                    onChange={handleInputChange}
+                    value={name}
+                />
+                <label htmlFor='email'>Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter Email ..."
+                    onChange={handleInputChange}
+                    value={email}
+                />
+                <label htmlFor='contact'>Contact</label>
+                <input
+                    type="number"
+                    id="contact"
+                    name="contact"
+                    placeholder="Enter Contact No. ..."
+                    onChange={handleInputChange}
+                    value={contact}
+                /> 
+            </form>
+        </div>
+    );
+};
 
 export default AddEdit
